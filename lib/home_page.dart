d93ddcdcc4ce24dc8f80ca3207dd6fe788c71a51
Name <user@example.com>
2023-07-04 22:23:19 +0530

    Day 2 work

---
+++ lib/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+  // ignore: non_constant_identifier_names
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Material(
+      child: Center(
+        child: Container(
+          child: Text("Hello"),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:catalog/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -9,13 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome her"),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
